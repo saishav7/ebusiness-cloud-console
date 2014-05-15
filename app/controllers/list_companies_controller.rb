4d047a41501e8323b18d642f1c2203e6c2bfc1cb
saishav <saishav7@gmail.com>
2014-05-15 23:19:57 +1000

    generated function to view comapny list

--- app/controllers/list_companies_controller.rb
+++ app/controllers/list_companies_controller.rb
@@ -3,6 +3,17 @@ class ListCompaniesController < ApplicationController
   def viewCompanyList
   
    conn = PGconn.open(:dbname => 'EBDX')
-   res = conn.exec('SELECT * FROM Company')   
+   res = conn.exec('SELECT * FROM Company')
+   @id1 = res[0]["company_id"]
+   @name1 = res[0]["company_name"]
+   @status1 = res[0]["company_status_id"]
+
+   @id2 = res[1]["company_id"]
+   @name2 = res[1]["company_name"]
+   @status2 = res[1]["company_status_id"]
+
+   @id3 = res[2]["company_id"]
+   @name3 = res[2]["company_name"]
+   @status3 = res[2]["company_status_id"]
   end
 end
