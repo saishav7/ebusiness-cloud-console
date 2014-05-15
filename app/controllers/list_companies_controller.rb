3233f1847c19e44c469a39320dd383f717215aff
saishav <saishav7@gmail.com>
2014-05-15 17:17:54 +1000

    database connection successful

--- app/controllers/list_companies_controller.rb
+++ app/controllers/list_companies_controller.rb
@@ -1,4 +1,8 @@
+require 'pg'
 class ListCompaniesController < ApplicationController
   def viewCompanyList
+  
+   conn = PGconn.open(:dbname => 'EBDX')
+   res = conn.exec('SELECT * FROM Company')   
   end
 end
